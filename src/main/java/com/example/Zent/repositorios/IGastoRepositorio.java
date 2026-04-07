64abc543ecde80f11265177531ac4f521b499967
camila ortiz <camilalampion97@gmail.com>
2026-04-07 12:42:47 -0500

    feat: actualizar repositorios de usuario/gastos y avance en servicios [WIP]
    • WIP: Creación inicial de la capa de servicios de usuario (pendiente por completar lógica de negocio).
    • Actualización y optimización de los repositorios de Usuario y Gastos.
    • Reorganización general de la estructura del código.

---
+++ src/main/java/com/example/Zent/repositorios/IGastoRepositorio.java
@@ -0,0 +1,14 @@
+package com.example.Zent.repositorios;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Zent.modelos.Gasto;
+
+
+public interface IGastoRepositorio extends JpaRepository<Gasto, Integer> {
+    List<Gasto> findByValor(Double valor);
+    List<Gasto> findByFecha(LocalDate fecha);
+}
